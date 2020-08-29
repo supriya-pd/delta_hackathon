10392d91ee5759b6d618374ba3258ef84a10c362
supriya-pd <59952449+supriya-pd@users.noreply.github.com>
2020-08-29 16:06:04 +0530

    after drawing you can save image to gallery

--- app/src/main/java/com/example/imgeditor/EditActivity.java
+++ app/src/main/java/com/example/imgeditor/EditActivity.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.UUID;
 
 public class EditActivity extends AppCompatActivity {
 
     EditView editView;
+    Button saveBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        editView=new EditView(this);
-        setContentView(editView);
+        setContentView(R.layout.activity_edit);
+        editView=findViewById(R.id.customView);
+        saveBTN=findViewById(R.id.saveBTN);
 
         Intent intent=getIntent();
         String path=intent.getStringExtra("position");
@@ -24,7 +32,29 @@ public class EditActivity extends AppCompatActivity {
         Drawable d=Drawable.createFromPath(path);
         editView.setBackground(d);
 
-        
+
+        saveBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                editView.setDrawingCacheEnabled(true);
+
+                String imgSaved = MediaStore.Images.Media.insertImage(
+                        getContentResolver(), editView.getDrawingCache(),
+                        UUID.randomUUID().toString()+".png", "drawing");
+                //feedback
+                if(imgSaved!=null){
+                    Toast savedToast = Toast.makeText(getApplicationContext(),
+                            "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                    savedToast.show();
+                }
+                else{
+                    Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                            "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                    unsavedToast.show();
+                }
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/imgeditor/EditView.java
+++ app/src/main/java/com/example/imgeditor/EditView.java
@@ -1,9 +1,13 @@
 package com.example.imgeditor;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -11,6 +15,13 @@ import androidx.annotation.Nullable;
 public class EditView extends View {
 
     Paint brush;
+    private Canvas mExtraCanvas;
+    private Bitmap mExtraBitmap;
+    private Path mPath;
+    private static final float TOUCH_TOLERANCE = 4;
+    private float mX, mY;
+
+
     public EditView(Context context) {
         super(context);
         init();
@@ -29,12 +40,85 @@ public class EditView extends View {
 
    public void init(){
 
+       mPath = new Path();
+
    brush=new Paint(Paint.ANTI_ALIAS_FLAG);
    brush.setStyle(Paint.Style.STROKE);
    brush.setColor(Color.MAGENTA);
    brush.setStrokeWidth(10);
+   brush.setDither(true);
+   brush.setStrokeJoin(Paint.Join.ROUND);
+   brush.setStrokeCap(Paint.Cap.ROUND);
 
     }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
 
+        mExtraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mExtraCanvas = new Canvas(mExtraBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(mExtraBitmap, 0, 0, null);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float x = event.getX();
+        float y = event.getY();
+
+
+        utility(event, x, y);
+
+        return true;
+
+    }
+
+    public void utility(MotionEvent event, float x, float y) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchStart(x, y);
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touchMove(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touchUp();
+
+                break;
+            default:
+        }
+    }
+
+    private void touchStart(float x, float y) {
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    private void touchMove(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+
+            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+
+            mX = x;
+            mY = y;
+
+            mExtraCanvas.drawPath(mPath,brush);
+        }
+    }
+
+    private void touchUp() {
+
+        mPath.reset();
+    }
 }
